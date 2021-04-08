48b3cd4bf3daea4859210ec1c8fd1e0d7f085b07
jenna-mae <80732297+jenna-mae@users.noreply.github.com>
2021-04-07 17:05:47 -0700

    updated sql file

--- database/openspaceDb.sql
+++ database/openspaceDb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Apr 07, 2021 at 05:37 PM
+-- Generation Time: Apr 08, 2021 at 12:04 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.4.2
 
@@ -37,7 +37,9 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `email`, `firstName`, `lastName`, `username`, `password`, `birthday`, `gender`) VALUES
 (8, 'test@email.com', 'Test', 'Test', 'Test', '$2y$10$OYbzAHVdKV24JKcyZXtar.hLOVRBO2311abgDEl7FZSvtM6tQ.9By', '1997-08-10', 'Male'),
-(9, 'jenna@email.com', 'Jenna', 'Emerman', 'Jenna', '$2y$10$KJVc9V5xNrtbdg4mtiEuE.l4iYXXbJmqZ.qtSVhfYoa27e0tffwsS', '1997-08-10', 'Female');
+(9, 'jenna@email.com', 'Jenna', 'Emerman', 'Jenna', '$2y$10$KJVc9V5xNrtbdg4mtiEuE.l4iYXXbJmqZ.qtSVhfYoa27e0tffwsS', '1997-08-10', 'Female'),
+(10, 'sample@email.com', 'Sample', 'Sample', 'Sample', '$2y$10$YbJHHAL2d.dJHk84lRgUeOkBQvyg4GXr4pBWgU3JyL.HiiWAa7eiq', '2021-04-07', 'NA'),
+(11, 'xx@email.com', 'xx', 'xx', 'xx', '$2y$10$EiqYIDPkhApSu3X4eGSN1eXaexokX.duV/PkisLx.A64ZuBkNY2cK', '1997-08-10', 'Male');
 
 --
 -- Indexes for dumped tables
@@ -57,4 +59,4 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
