5992275a93becddd46bcb471cd022e58516820e9
jenna-mae <80732297+jenna-mae@users.noreply.github.com>
2021-04-21 20:45:52 -0700

    Jenna minor changes

--- classes/Space.php
+++ classes/Space.php
@@ -2,8 +2,6 @@
 
 class Space {
     public function create($name, $category, $date, $description, $host, $startTime, $endTime, $link) {
-        // $active, $link, $recent
-        echo "hey";
         $space = Db::DoQuery("INSERT INTO spaces(name, category, date, description, host, startTime, endTime, link) VALUES ('".$name."', '".$category."','".$date."', '".$description."', '".$host."', '".$startTime."', '".$endTime."', '".$link."')");
         echo"INSERT INTO spaces(name, category, date, description, host, startTime, endTime, link) VALUES ('".$name."', '".$category."','".$date."', '".$description."', '".$host."', '".$startTime."', '".$endTime."', '".$link."')";
         return $space;
