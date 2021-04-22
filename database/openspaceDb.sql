10c0459cf8537662eb2ebd46a6c1525e9b2a148e
jenna-mae <80732297+jenna-mae@users.noreply.github.com>
2021-04-21 20:16:39 -0700

    Jenna updated sql file

--- database/openspaceDb.sql
+++ database/openspaceDb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Apr 21, 2021 at 12:55 AM
+-- Generation Time: Apr 22, 2021 at 03:14 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.4.2
 
@@ -13,6 +13,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `openspace`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Hangout'),
+(2, 'Art'),
+(3, 'Sports'),
+(4, 'Books'),
+(5, 'Games');
 
 -- --------------------------------------------------------
 
@@ -42,9 +64,39 @@ INSERT INTO `spaces` (`id`, `name`, `category`, `recentlyAdded`, `description`, 
 (4, 'test1', 1, 0, 't', 15, '2021-04-21', '20:42:00', '22:42:00', 'https://www.freshprep.ca/menu/this-week'),
 (5, 'test2', 5, 0, 'test2', 15, '2021-04-30', '17:57:00', '20:54:00', 'https://www.freshprep.ca/menu/this-week');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `birthday` date NOT NULL,
+  `gender` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `firstName`, `lastName`, `username`, `password`, `birthday`, `gender`) VALUES
+(15, 'user@email.com', 'user', 'user', 'user', '$2y$10$K5JhTfDpzqb27Fn2IB/MF.Tao6ONUj7NU.ZZKU2rBhrBms6w70aF2', '2021-04-12', 'NA');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `spaces`
@@ -55,14 +107,32 @@ ALTER TABLE `spaces`
   ADD KEY `host` (`host`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `spaces`
 --
 ALTER TABLE `spaces`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Constraints for dumped tables
