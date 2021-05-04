b1fb30d877236017cc1144fafdd941c04b3eb3a5
jenna-mae <80732297+jenna-mae@users.noreply.github.com>
2021-05-03 21:37:27 -0700

    Jenna made old spaces become inactive

--- classes/Space.php
+++ classes/Space.php
@@ -1,9 +1,19 @@
 <?php
 
 class Space {
+    public function __construct() {
+        // For Now this only makes the Space go inactive after the date has passed. Next I will make it so it happens after the time has passed.
+        $results = Db::getAllEntries("SELECT * FROM spaces");
+        date_default_timezone_set('America/Vancouver');
+        foreach($results as $result) {
+            // && date("H:i a")>=$result["endTime"]
+            if(date("Y-m-d")>=$result["date"]) {
+                Db::DoQuery("UPDATE spaces SET isactive = 0 WHERE id=".$result["id"]);
+            } 
+        }
+    }
     public function create($name, $category, $date, $description, $host, $startTime, $endTime, $link) {
         $space = Db::DoQuery("INSERT INTO spaces(name, category, date, description, host, startTime, endTime, link) VALUES ('".$name."', '".$category."','".$date."', '".$description."', '".$host."', '".$startTime."', '".$endTime."', '".$link."')");
-        echo"INSERT INTO spaces(name, category, date, description, host, startTime, endTime, link) VALUES ('".$name."', '".$category."','".$date."', '".$description."', '".$host."', '".$startTime."', '".$endTime."', '".$link."')";
         return $space;
     }
 
